f2b3e03c6e2a1890070af2678d6310f7274e9acc
luciapuccini <puccinilucia@gmail.com>
2021-06-06 13:52:38 -0300

    Add build

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import styled from 'styled-components';
 import Map from '../components/atoms/Map';
 import ProductsSection from '../components/molecules/ProductsSection';
